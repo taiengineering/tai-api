8213acaefc8885223fe1e70ae5df3e71d5dcd170
taiengineering <taiengcokr@gmail.com>
2026-03-12 12:01:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from routers.areas import router as areas_router
 from routers.equipment_assets import router as assets_router
 from routers.inspection_sets import router as inspection_sets_router
 from routers.work_schedules import router as work_schedules_router
+from routers.schedule_engine import router as schedule_engine_router
 
 app = FastAPI(
     title="TAI API",
@@ -22,7 +23,7 @@ app.include_router(areas_router)
 app.include_router(assets_router)
 app.include_router(inspection_sets_router)
 app.include_router(work_schedules_router)
-
+app.include_router(schedule_engine_router)
 
 @app.get("/")
 def root():
