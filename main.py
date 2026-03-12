12c4bea2297eefa8bff5a716738736e197ba8c6f
taiengineering <taiengcokr@gmail.com>
2026-03-12 11:31:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from routers.companies import router as companies_router
 from routers.factories import router as factories_router
 from routers.buildings import router as buildings_router
+from routers.areas import router as areas_router
+from routers.equipment_assets import router as assets_router
 
 app = FastAPI(
     title="TAI API",
@@ -14,6 +16,8 @@ app = FastAPI(
 app.include_router(companies_router)
 app.include_router(factories_router)
 app.include_router(buildings_router)
+app.include_router(areas_router)
+app.include_router(assets_router)
 
 
 @app.get("/")
