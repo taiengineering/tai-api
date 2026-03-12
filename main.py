030325d8a520bd62c6cbd18a340eb9c6d4fba66a
taiengineering <taiengcokr@gmail.com>
2026-03-12 11:18:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-from routers.companies import router as companies_router
 
-app = FastAPI()
+from routers.companies import router as companies_router
+from routers.factories import router as factories_router
+
+app = FastAPI(
+    title="TAI API",
+    version="1.0"
+)
 
 app.include_router(companies_router)
+app.include_router(factories_router)
+
 
 @app.get("/")
 def root():
