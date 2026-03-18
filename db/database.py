7289dacc6226aef4a3ef76a41912ccdb390fa9ae
taiengineering <taiengcokr@gmail.com>
2026-03-18 10:30:39 +0900

    Rename SUPABASE_SERVICE_KEY to SUPABASE_KEY

--- db/database.py
+++ db/database.py
@@ -5,8 +5,8 @@ import os
 from supabase import create_client, Client
 
 SUPABASE_URL = os.environ.get("https://xntdkrjhgcscmqctdzyo.supabase.co")
-SUPABASE_SERVICE_KEY = os.environ.get("sb_secret_sb_secret_fBeYn64yjsdUuucIjGQu0Q_ZSkVqYfx")
+SUPABASE_KEY = os.environ.get("sb_secret_sb_secret_fBeYn64yjsdUuucIjGQu0Q_ZSkVqYfx")
 
 def get_supabase() -> Client:
-    return create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
+    return create_client(SUPABASE_URL, SUPABASE_KEY)
 
